97a6484d557c23e3dbbce59e0aee54949ad11137
Gauravthorat30 <gauravthorat42057@gmail.com>
2025-12-03 09:41:56 +0530

    modifying project 4

--- 4-GuessTheNumber/chaiaurcode.js
+++ 4-GuessTheNumber/chaiaurcode.js
@@ -1,4 +1,6 @@
 const submit = document.querySelector("#subt");
 const form = document.querySelector(".form");
+const guessField = document.querySelector("#guessField");
+const subt = document.querySelector("#subt");
 const answer = Math.floor(Math.random() * 100 + 1);
 console.log(answer);
