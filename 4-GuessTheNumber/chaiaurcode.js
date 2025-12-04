6f1a61d9d9a471b4a5f95beaebda792a35fe6a87
Gauravthorat30 <gauravthorat42057@gmail.com>
2025-12-04 13:09:58 +0530

    project 4 processing

--- 4-GuessTheNumber/chaiaurcode.js
+++ 4-GuessTheNumber/chaiaurcode.js
@@ -1,6 +1,31 @@
-const submit = document.querySelector("#subt");
+const answer = Math.floor(Math.random() * 100 + 1);
+
 const form = document.querySelector(".form");
 const guessField = document.querySelector("#guessField");
 const subt = document.querySelector("#subt");
-const answer = Math.floor(Math.random() * 100 + 1);
-console.log(answer);
+const resultParas = document.querySelector(".resultParas");
+const guesses = document.querySelector(".guesses");
+const lastResult = document.querySelector(".lastResult");
+const lowOrHi = document.querySelector(".lowOrHi");
+
+const p = document.createElement("p");
+
+let prevGuesses = [];
+let numOfGuesses = 10;
+let playGame = true;
+
+subt.addEventListener("click", function (e) {
+  e.preventDefault();
+  if (!playGame) {
+    return;
+  }
+  if (!(guessField.value > -1 && 101 > guessField.value)) {
+    lowOrHi.innerHTML = "please put vaue a valid value";
+    return;
+  }
+  numOfGuesses--;
+  lastResult.innerHTML = numOfGuesses;
+  prevGuesses.push(guessField.value);
+  guesses.innerHTML = prevGuesses;
+  console.log(answer);
+});
